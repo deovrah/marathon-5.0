27f3aadd7a2a3c3c771b37aa37a814aeb2c241ba
deovrah <deovrah@yandex.ru>
2023-01-04 01:04:50 +0300

    Базовая стилизация главной страницы макета

---
+++ scripts/slider.js
@@ -0,0 +1,15 @@
+var slideIndex = 1;
+showSlide(slideIndex);
+
+function moveSlide(n) {
+	showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+	var i;
+	var slides = document.getElementsByClassName("script-item");
+	if (n > slides.length) { slideIndex = 1; }
+	if (n < 1) { slideIndex = slides.length; }
+	for (i = 0; i < slides.length; i++) { slides[i].style.display="none"; }
+	slides[slideIndex-1].style.display="grid";
+}
